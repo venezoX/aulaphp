0da9ac4d2838b793d071168791cf236dfdb64ef4
venezoX <otto.arrue@gmail.com>
2022-09-03 22:45:32 -0300

    atualização

---
+++ primos.php
@@ -0,0 +1,25 @@
+<?php
+$numeroprimo = [];
+
+for($i = 1; $i <= 28 ; $i++){
+  //  echo $i .',';
+        $primo = true;
+    for($n = 2; $n < $i; $n++){
+        if($i % $n == 0){
+            $primo = false;
+            break;
+        }
+    }
+
+    if($primo){
+        array_push($numeroprimo,$i);
+    }
+}
+print_r($numeroprimo).'<br>';
+
+//===========================================================\\
+
+
+
+
+
