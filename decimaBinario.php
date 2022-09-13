da2cdb3d75804012c5e08d32a3043947928ca167
venezoX <otto.arrue@gmail.com>
2022-09-12 23:45:50 -0300

    subindo arquivo de conversao para binarios

--- decimaBinario.php
+++ decimaBinario.php
@@ -1,5 +1,26 @@
 <?php
 
-$decimal = 10;
+$n = 483;
+$binariosArmazem = array();
+$n = $div;
 
-    
+
+
+    do {
+        $resto = $n % 2;
+        $div = $n / 2;
+        if ($resto == 1 || $resto == 0){
+    array_push($bi,$resto);
+        }
+
+    }
+
+while ($div > 1);
+
+$rev = array_reverse($bi);
+
+foreach ($rev as $bin)
+
+{
+    echo $bin;
+}
