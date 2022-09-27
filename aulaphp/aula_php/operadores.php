82bc8fb14e9bc5af13f4ef60914a8ef41c048961
venezoX <otto.arrue@gmail.com>
2022-09-27 19:35:02 -0300

    Exemplo de operadores logicos

---
+++ aulaphp/aula_php/operadores.php
@@ -0,0 +1,23 @@
+<?php
+
+$x = 1;
+echo $x / 4; //operação basica
+
+
+$y = 10;
+echo $y % 3;
+
+$y -= 3;
+echo $y; 
+
+$nome = "otto";
+
+$sobrenome = " arrué";
+echo $nome . $sobrenome;
+
+//com atribuição
+
+$z = 10;
+$z += 2;
+echo $z;
+
