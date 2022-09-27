95989a2327221d35aca8e4f9abe1438bb8fcb13f
venezoX <otto.arrue@gmail.com>
2022-09-27 19:17:49 -0300

    Atividade de jokempo 2

--- curso_php_infoserv/jokempo2.php
+++ curso_php_infoserv/jokempo2.php
@@ -4,15 +4,19 @@ $alternativa1 = "pedra";
 $alternativa2 = "papel";
 $alternativa3 = "tesoura";
 
-$spock=$alternativa2;
-$lagarto = $alternativa1;
+$spock=$alternativa1;
+$lagarto = $alternativa2;
+
+
 
   if($spock == $alternativa2 && $lagarto == $alternativa1 || $spock == $alternativa1 && $lagarto == $arternativa3)
   {
-    Echo " O jogador Spock escolheu $spock e venceu do $lagarto";
+    Echo " O jogador Spock escolheu $spock e venceu $lagarto";
   }
 
-  elseif ($lagarto == $alternativa2 && $spock  == $alternativa1 || $lagarto == $alternativa && $spock == $arternativa)
+  elseif ($lagarto == $alternativa2 && $spock  == $alternativa1 || $lagarto == $alternativa3 && $spock == $arternativa2 || $lagarto == $spock)
   {
-    echo $lagarto;
+    echo " O jogador Lagarto escolheu $lagarto e venceu $spock";
   } 
+
+  // spock - pedra|papel  lagarto - spock|tesoura
