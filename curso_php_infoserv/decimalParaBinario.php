7268ad8998239e6cd8537d092d1579e97ed61ae7
venezoX <otto.arrue@gmail.com>
2022-09-26 22:44:04 -0300

    tarefa de decimal para binários

---
+++ curso_php_infoserv/decimalParaBinario.php
@@ -0,0 +1,26 @@
+<?php
+
+$numero = 300;
+$armazemBinario = array();
+
+
+
+do  
+{
+    $resto = $numero % 2;
+    $div = $numero / 2;
+    $numero = $div;
+    if ($resto == 1 || $resto == 0)
+    array_push($armazemBinario,$resto);
+}
+
+while ($div > 1);
+for ($i=(count($armazemBinario)-1);$i>=0;$i--)
+{
+    $contrario[] = $armazemBinario[$i];
+}
+foreach ($contrario as $binarioReverso) {
+    echo $binarioReverso;
+}
+echo "$binarioReverso  é o binario do número $numero";
+
