d387ada993fd7cfb7925611cb40c8a417a4f4ebd
venezoX <49325906+venezoX@users.noreply.github.com>
2022-09-27 12:42:22 -0300

    continuação da tarefa de jokempo, inacabado

--- curso_php_infoserv/jokempo2.php
+++ curso_php_infoserv/jokempo2.php
@@ -1,5 +1,10 @@
 <?php
 
-$opção1 = "papel";
-$opção2 = "pedra";
-$opção3 = "tesoura";
+$alternativa1 = "pedra";
+$alternativa2 = "papel";
+$alternativa3 = "tesoura";
+
+$spock=$alternativa1;
+$lagarto = $alternativa3;
+
+  if($spock == $opcao2 && $lagarto == $alternativa3 || ){}
