7153539fa568be1b1d747f8e7dfef7aa2bcacefe
venezoX <otto.arrue@gmail.com>
2022-09-27 17:26:53 -0300

    Atividade de jokempo 2 incompleta

--- curso_php_infoserv/jokempo2.php
+++ curso_php_infoserv/jokempo2.php
@@ -4,7 +4,15 @@ $alternativa1 = "pedra";
 $alternativa2 = "papel";
 $alternativa3 = "tesoura";
 
-$spock=$alternativa1;
-$lagarto = $alternativa3;
+$spock=$alternativa2;
+$lagarto = $alternativa1;
 
-  if($spock == $opcao2 && $lagarto == $alternativa3 || ){}
+  if($spock == $alternativa2 && $lagarto == $alternativa1 || $spock == $alternativa1 && $lagarto == $arternativa3)
+  {
+    Echo " O jogador Spock escolheu $spock e venceu do $lagarto";
+  }
+
+  elseif ($lagarto == $alternativa2 && $spock  == $alternativa1 || $lagarto == $alternativa && $spock == $arternativa)
+  {
+    echo $lagarto;
+  } 
